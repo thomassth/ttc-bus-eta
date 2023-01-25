3405f745726091e02aa22015ae00fbd51c120388
Thomas Kam <thomassth@users.noreply.github.com>
2023-01-25 00:56:15 -0500

    coloring improvement

--- src/styles/FluentTheme.tsx
+++ src/styles/FluentTheme.tsx
@@ -33,51 +33,35 @@ export function FluentTheme({ children }: { children: JSX.Element }) {
   );
 }
 
-export const ttcEtaLightTheme: Theme & {} = {
-  ...webLightTheme,
+const ttcPalate = {
   // Title
-  colorBrandForegroundLink: "#9c1b14",
+  colorBrandForegroundLink: "#ae1e17",
   colorBrandForegroundLinkHover: "#ae1e17",
-  colorBrandForegroundLinkPressed: "#841711",
+  colorBrandForegroundLinkPressed: "#5e110c",
   // icon colos on hover
   colorNeutralForeground2BrandHover: "#ae1e17",
-  colorNeutralForeground2BrandPressed: "#841711",
+  colorNeutralForeground2BrandPressed: "#5e110c",
   // buttons, badge
   colorBrandBackground: "#9c1b14",
-  colorBrandBackgroundHover: "#ae1e17",
-  colorBrandBackgroundPressed: "#841711",
+  colorBrandBackgroundHover: "#70130e",
+  colorBrandBackgroundPressed: "#5e110c",
   // transparent badge with colored outline
-  colorBrandForeground1: "#9c1b14",
+  colorBrandForeground1: "#f54747",
   // radio button; radio button border
   colorCompoundBrandForeground1: "#9c1b14",
   colorCompoundBrandForeground1Hover: "#ae1e17",
-  colorCompoundBrandForeground1Pressed: "#841711",
+  colorCompoundBrandForeground1Pressed: "#5e110c",
   colorCompoundBrandStroke: "#9c1b14",
   colorCompoundBrandStrokeHover: "#ae1e17",
-  colorCompoundBrandStrokePressed: "#841711",
+  colorCompoundBrandStrokePressed: "#5e110c",
+};
+
+export const ttcEtaLightTheme: Theme & {} = {
+  ...webLightTheme,
+  ...ttcPalate,
 };
 
 export const ttcEtaDarkTheme: Theme & {} = {
   ...webDarkTheme,
-  // Title
-  colorBrandForegroundLink: "#f54747",
-  colorBrandForegroundLinkHover: "#f56161",
-  colorBrandForegroundLinkPressed: "#de2929",
-  // icon colos on hover
-  colorNeutralForeground2BrandHover: "#f56161",
-  colorNeutralForeground2BrandPressed: "#de2929",
-  // buttons, badge
-  colorBrandBackground: "#9c1b14",
-  colorBrandBackgroundHover: "#f56161",
-  colorBrandBackgroundPressed: "#de2929",
-  // transparent badge with colored outline
-  colorBrandForeground1: "#f55f47",
-  // radio button; radio button border
-  colorCompoundBrandForeground1: "#f54747",
-  colorCompoundBrandForeground1Hover: "#f56161",
-  colorCompoundBrandForeground1Pressed: "#de2929",
-  // also on search bar deco
-  colorCompoundBrandStroke: "#f54747",
-  colorCompoundBrandStrokeHover: "#f56161",
-  colorCompoundBrandStrokePressed: "#de2929",
+  ...ttcPalate,
 };
