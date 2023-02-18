e64b34691cc163660fc62ef730644e4118b726b2
Thomas Kam <thomassth@users.noreply.github.com>
2023-02-18 01:53:51 -0500

    Type fix (#75)
    * remove unused tests
    
    * import and type update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router-dom";
 
 import { store } from "./app/store";
 import { saveStopBookmarkState } from "./features/bookmarks/localstorage";
