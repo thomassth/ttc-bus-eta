0e20d1f51ba26054cde07422d069322c976e43b4
HoiPang2 <hoipang1e06@gmail.com>
2023-02-21 23:49:39 -0500

    feat: update StopAccordions

--- src/components/accordions/StopAccordions.tsx
+++ src/components/accordions/StopAccordions.tsx
@@ -4,40 +4,39 @@ import {
   AccordionPanel,
   Badge,
 } from "@fluentui/react-components";
+import { useCallback } from "react";
 
-import { LineStopElement } from "../../models/lineStop";
+import { StopAccordionsParams } from "../../models/lineStop";
 import { fluentStyles } from "../../styles/fluent";
-import { parseRoute } from "../parser/routeName";
+import { removeSpecialChars } from "../utils/routeNameUtils";
 
-export function StopAccordions(props: {
-  result: LineStopElement[];
-  title: string;
-  direction: string;
-  lineNum: number;
-  tag: string;
-}) {
+export function StopAccordions(props: StopAccordionsParams) {
   const fluentStyle = fluentStyles();
 
-  const stops = props.result.map((lineStop) => {
-    return (
-      <li key={`${props.lineNum}-${props.direction}-${lineStop.key}`}>
-        <AccordionPanel className={fluentStyle.accordionPanel}>
-          <div className="lineDetails">{lineStop.stopId} </div>
-          <div className="lineDetails">{lineStop.latlong} </div>
-          <div className="lineDetails">{lineStop.name}</div>
-        </AccordionPanel>
-      </li>
-    );
-  });
+  const StopsDetails = useCallback(() => {
+    const stops = props.result.map((lineStop) => {
+      return (
+        <li key={`${props.lineNum}-${props.direction}-${lineStop.key}`}>
+          <AccordionPanel className={fluentStyle.accordionPanel}>
+            <div className="lineDetails">{lineStop.stopId} </div>
+            <div className="lineDetails">{lineStop.latlong} </div>
+            <div className="lineDetails">{lineStop.name}</div>
+          </AccordionPanel>
+        </li>
+      );
+    });
+
+    return <ul>{stops}</ul>;
+  }, [props]);
 
   return (
     <AccordionItem value={props.tag}>
       <AccordionHeader className={fluentStyle.accordionHeader}>
         <Badge className={fluentStyle.badge}>{props.direction}</Badge>
         <Badge className={fluentStyle.badge}>{props.lineNum}</Badge>
-        {parseRoute(props.title)}
+        {removeSpecialChars(props.title)}
       </AccordionHeader>
-      <ul>{stops}</ul>
+      <StopsDetails />
     </AccordionItem>
   );
 }
