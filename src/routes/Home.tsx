5ca61462718b17179307d9a50198acc86e59004e
thomassth <thomassth@users.noreply.github.com>
2023-06-20 02:29:30 -0400

    search for stop

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,45 +1,11 @@
-import { Button, Input } from "@fluentui/react-components";
-import { SetStateAction, useCallback, useState } from "react";
-import { useTranslation } from "react-i18next";
-
 import FavouriteEta from "../components/eta/FavouriteEta";
-import { fluentStyles } from "../styles/fluent";
-import useNavigate from "./navigate";
+import Search from "./Search";
 
 export default function Home() {
-  const [input, setInput] = useState("");
-  const { t } = useTranslation();
-  const { navigate } = useNavigate();
-
-  const handleLineChange = useCallback(
-    (e: { currentTarget: { value: SetStateAction<string> } }) => {
-      setInput(e.currentTarget.value);
-    },
-    []
-  );
-
-  const handleSearchClick = useCallback(() => {
-    if (input === "") return;
-
-    navigate(`lines/${input}`);
-  }, [input]);
-
-  const fluentStyle = fluentStyles();
 
   return (
     <main className="homePage">
-      <form className="searchBlock">
-        <Input
-          value={input}
-          className={fluentStyle.flexGrowContent}
-          onChange={handleLineChange}
-          aria-label={t("lines.ariaLabel") || ""}
-          placeholder={t("lines.placeholder") || ""}
-        />
-        <Button appearance="primary" type="submit" onClick={handleSearchClick}>
-          {t("buttons.search")}
-        </Button>
-      </form>
+      <Search />
       <FavouriteEta />
     </main>
   );
---
+++ src/routes/Search.tsx
@@ -0,0 +1,68 @@
+import { Button, Input } from "@fluentui/react-components";
+import { SetStateAction, useCallback, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { fluentStyles } from "../styles/fluent";
+// import { useParams } from "react-router-dom";
+import useNavigate from "./navigate";
+
+
+export default function NewVehicle() {
+    const [lineInput, setLineInput] = useState("");
+    const [stopInput, setStopInput] = useState("")
+    const { t } = useTranslation();
+    const { navigate } = useNavigate();
+
+    const handleLineChange = useCallback(
+        (e: { currentTarget: { value: SetStateAction<string> } }) => {
+            setLineInput(e.currentTarget.value);
+        },
+        []
+    );
+
+    const handleStopChange = useCallback(
+        (e: { currentTarget: { value: SetStateAction<string> } }) => {
+            setStopInput(e.currentTarget.value);
+        },
+        []
+    );
+
+    const handleSearchClick = useCallback(() => {
+        if (stopInput !== "") {
+            console.log(stopInput)
+            navigate(`stops/${stopInput}`)
+
+        }
+        else if (lineInput !== "") navigate(`lines/${lineInput}`);
+    }, [lineInput, stopInput]);
+
+    const fluentStyle = fluentStyles();
+    return (
+        <form className="searchForm">
+            <div className="nextVehicleContainer">
+                <h1>Next Vehicle</h1>
+                <div>
+                    <Input
+                        size="large"
+                        appearance="underline"
+                        type="number"
+                        onChange={handleStopChange}
+                        value={stopInput}
+                        placeholder={t("stops.ariaLabel") || ""} />
+                </div>
+            </div>
+            <p className="separation"><hr /> or <hr /> </p>
+            <div className="searchBlock">
+                <Input
+                    value={lineInput}
+                    className={fluentStyle.flexGrowContent}
+                    onChange={handleLineChange}
+                    aria-label={t("lines.ariaLabel") || ""}
+                    placeholder={`${t("lines.placeholder")}`}
+                />
+                <Button appearance="primary" type="submit" onClick={handleSearchClick}>
+                    {t("buttons.search")}
+                </Button>
+            </div>
+        </form>
+    );
+}
