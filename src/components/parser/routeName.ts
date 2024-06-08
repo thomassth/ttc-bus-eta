46fc0a23d27bf77408762601c281750f449c24c6
Thomas Kam <thomassth@users.noreply.github.com>
2024-06-08 00:14:06 -0400

    regex tweak

--- src/components/parser/routeName.ts
+++ src/components/parser/routeName.ts
@@ -3,7 +3,7 @@ export function parseRoute(routeName: string): {
   prefix?: string;
 } {
   // remove line number, directions
-  const dirAndTagRegex = /^([\w\d\s]+)-([/\w\d\s-]+)$/;
+  const dirAndTagRegex = /^([\w\s]+)\s*-\s*(\d[\w]+\s)*([/\w\s-]+)$/;
   // remove line number
   const tagRegex = /\w+-/i;
   if (dirAndTagRegex.test(routeName)) {
@@ -11,7 +11,7 @@ export function parseRoute(routeName: string): {
     if (directionAndName)
       return {
         prefix: directionAndName[1].trim(),
-        name: directionAndName[2].trim(),
+        name: directionAndName[3].trim(),
       };
   }
   return { name: routeName.replace(tagRegex, "") };
