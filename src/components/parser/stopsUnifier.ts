d102792aa7c4f0e0e7ff183f984451057f461669
thomassth <thomassth@users.noreply.github.com>
2024-06-22 00:33:40 -0400

    algo fix

--- src/components/parser/stopsUnifier.ts
+++ src/components/parser/stopsUnifier.ts
@@ -1,46 +1,43 @@
-export function mergeAndGroup(...arrays: number[][]) {
-  // Find the common elements across all arrays
+export function mergeAndGroup(...arrays: number[][]): (number | number[][])[] {
+  // Step 1: Find the common elements across all arrays
   const common = arrays.reduce((acc, arr) =>
     acc.filter((x) => arr.includes(x))
   );
-  const result: number[] = [];
+
+  if (common.length === 0) {
+    return [arrays];
+  }
   const start: number[][] = [];
   const end: number[][] = [];
 
-  arrays.forEach((arr) => {
-    let group: number[] = [];
-    arr.forEach((item) => {
-      if (common.includes(item)) {
-        if (group.length) {
-          // Check if this common item is the first one to be pushed into result
-          if (!result.includes(item)) {
-            result.push(...group);
-            group = [];
-          } else {
-            // Insert the group before the common item
-            start.push(group);
-            group = [];
-          }
-        }
-        if (!result.includes(item)) {
-          result.push(item);
-        }
-      } else {
-        group.push(item);
-      }
-    });
-    if (group.length) {
-      // Check if the array ends with a group
-      if (Array.isArray(result[result.length - 1])) {
-        start.push(group);
-      } else {
-        end.push(group);
-      }
-    }
-  });
+  // Step 2: Separate the front and end of the arrays
+  function separator(fullArray: number[], commonArray: number[]) {
+    const startIndex = fullArray.findIndex((item) => item === commonArray[0]);
+
+    const beforeCommon = fullArray.slice(0, startIndex);
+    if (beforeCommon.length > 0) start.push(beforeCommon);
+
+    const afterCommon = fullArray.slice(startIndex + commonArray.length);
+    if (afterCommon.length > 0) end.push(afterCommon);
+  }
+
+  arrays.forEach((array) => separator(array, common));
+
+  // Step 3: Push the combined array; also loop check the before & after areas
   const combined = [];
 
-  combined.push(start, ...result, end);
+  if (start.length === 1 && Array.isArray(start[0])) {
+    combined.push(start);
+  } else if (start.length > 0) {
+    combined.push(...mergeAndGroup(...start));
+  }
+  combined.push(...common);
+  if (end.length === 1 && Array.isArray(end[0])) {
+    combined.push(end);
+  }
+  if (end.length > 0) {
+    combined.push(...mergeAndGroup(...end));
+  }
 
   return combined;
 }
