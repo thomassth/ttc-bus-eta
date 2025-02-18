10e13d7ce8e2cefb7b15cd51c8adf2e3a663e778
thomassth <thomassth@users.noreply.github.com>
2025-02-18 04:08:18 -0500

    iteration

--- src/routes/Line.tsx
+++ src/routes/Line.tsx
@@ -1,13 +1,11 @@
 import { Accordion, Title1 } from "@fluentui/react-components";
-import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 
-import { SkeetList } from "../components/alerts/SkeetList.js";
+import { TtcAlertList } from "../components/alerts/TtcAlertList.js";
 import RouteInfo from "../components/fetch/FetchRoute.js";
 import SubwayRouteInfo from "../components/fetch/FetchSubwayRoute.js";
-import { ttcAlerts } from "../components/fetch/queries.js";
 
 export default function Line() {
   const params = useParams();
@@ -15,25 +13,13 @@ export default function Line() {
 
   const lineNum = parseInt(`${params.lineId}`);
 
-  const bskyAlerts = useQuery(ttcAlerts);
-
-  const filteredBskyAlerts =
-    bskyAlerts.data?.feed.filter(
-      (skeet: { post: { record: { text: string } } }) =>
-        lineNum < 6
-          ? skeet.post.record.text.match(`Line ${lineNum}`)
-          : skeet.post.record.text.startsWith(`${lineNum}`)
-    ) ?? [];
-
   useEffect(() => {
     document.title = t("lines.browserTitle", { lineNum });
   });
 
   return (
     <main className="line-page">
-      {filteredBskyAlerts.length > 0 && (
-        <SkeetList skeetList={filteredBskyAlerts} line={`${lineNum}`} />
-      )}
+      <TtcAlertList lineNum={[lineNum]} />
       <Title1>{t("lines.number", { lineNum })}</Title1>
       <Accordion defaultOpenItems collapsible>
         {lineNum < 6 && <SubwayRouteInfo line={lineNum} />}
