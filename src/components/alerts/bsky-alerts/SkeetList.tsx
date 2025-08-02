de395e550a8d30e203c1d7ae66dc7f65c29c7396
Paul Rupke <paul.rupke@gmail.com>
2025-08-02 13:47:27 -0400

    clean up function

--- src/components/alerts/bsky-alerts/SkeetList.tsx
+++ src/components/alerts/bsky-alerts/SkeetList.tsx
@@ -1,13 +1,10 @@
 import { type Skeet, SkeetElement } from "./Skeet.js";
 import style from "./SkeetList.module.css";
 
-const parseFullLineNumber = (skeets: Skeet[]) => {
-  if (skeets?.length === 0) {
-    return;
-  }
+const parseFullLineNumber = (skeet: Skeet) => {
   // Match the line number as well as any possible suffix (e.g. matches "96" or "96A")
-  const lineTextMatch = skeets[0].post.record.text.match(/^\d{1,3}\S/);
-  return lineTextMatch?.[0];
+  const lineTextMatch = skeet.post.record.text.match(/^\d{1,3}\S/);
+  return lineTextMatch?.[0] ?? null;
 };
 
 export const SkeetList = ({
@@ -19,11 +16,10 @@ export const SkeetList = ({
   line?: string;
   type?: string;
 }) => {
-  const lineNumber = parseFullLineNumber(skeetList);
+  const lineNumber =
+    skeetList?.length > 0 ? parseFullLineNumber(skeetList?.[0]) : line;
   const badgeArg =
-    line === "all"
-      ? { highlightAll: true }
-      : { line: typeof lineNumber === "string" ? lineNumber : line };
+    line === "all" ? { highlightAll: true } : { line: lineNumber };
   const dataArray = skeetList.map((skeet) => (
     <SkeetElement key={skeet.post.cid} skeet={skeet} badge={badgeArg} />
   ));
