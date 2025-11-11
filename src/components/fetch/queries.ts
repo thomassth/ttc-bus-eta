41be84c8d05af5ec8154125f687af17897860134
thomassth <8331853+thomassth@users.noreply.github.com>
2025-11-11 00:48:56 -0500

    updated GTFS-RT test implementations

--- src/components/fetch/queries.ts
+++ src/components/fetch/queries.ts
@@ -154,7 +154,9 @@ export const ttcVehicleLocation = (vehicle: number) =>
 // export const ttcGtfsAlerts = queryOptions({
 //   queryKey: ["ttc-gtfs-alerts"],
 //   queryFn: async () => {
-//     const response = await fetch("https://bustime.ttc.ca/gtfsrt/alerts");
+//     const response = await fetch(
+//       "https://gtfsrt.ttc.ca/alerts/all?format=binary"
+//     );
 //     if (!response.ok) {
 //       throw new Error("Network response was not ok");
 //     }
@@ -163,7 +165,25 @@ export const ttcVehicleLocation = (vehicle: number) =>
 //     const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
 //       new Uint8Array(buffer)
 //     );
-//     return feed.toJSON();
+//     return feed;
+//   },
+//   staleTime: 60 * 1000,
+//   refetchInterval: 60 * 1000,
+// });
+
+// export const ttcGtfsTripUpdate = queryOptions({
+//   queryKey: ["ttc-gtfs-trip-update"],
+//   queryFn: async () => {
+//     const response = await fetch("https://bustime.ttc.ca/gtfsrt/trips");
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+
+//     const buffer = await response.arrayBuffer();
+//     const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+//       new Uint8Array(buffer)
+//     );
+//     return feed;
 //   },
 //   staleTime: 60 * 1000,
 //   refetchInterval: 60 * 1000,
